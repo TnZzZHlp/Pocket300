ff57aff51fca7891fd79aed51ede8ae2bce63d4c
TnZzZHlp <tnzzzhlp@outlook.com>
2026-07-13 22:29:24 +0800

    test(api): add tests for post ratings parsing
    Add tests for parsePostRatings and parseRatedPostIds functions to verify
    HTML parsing of Discuz rating tables and rated post ID extraction.

--- app/src/test/java/com/yamibo/pocket300/api/YamiboPostsApiTest.kt
+++ app/src/test/java/com/yamibo/pocket300/api/YamiboPostsApiTest.kt
@@ -17,6 +17,8 @@ class YamiboPostsApiTest {
         assertFalse(page.pagination.hasNextPage)
         val post = page.posts.single()
         assertTrue(post.isOriginalPost)
+        assertTrue(page.thread.hasRatings)
+        assertEquals(4, post.ratingCount)
         assertEquals("<p>不可信正文</p>", post.html)
         assertEquals("https://bbs.yamibo.com/data/attachment/forum/202607/example.jpg", post.attachments.single().url)
         assertTrue(post.attachments.single().isImage)
@@ -96,12 +98,57 @@ class YamiboPostsApiTest {
         )
     }
 
+    @Test
+    fun parsesCompletePostRatingRows() {
+        val html = """
+            <table class="list">
+              <thead><tr><td>积分</td><td>用户名</td><td>时间</td><td>理由</td></tr></thead>
+              <tr>
+                <td>百合币 +3 枚</td>
+                <td><a href="space-uid-42.html">Alice &amp; Bob</a></td>
+                <td>2026-7-13 19:25</td>
+                <td>生日快乐 &amp; 好图</td>
+              </tr>
+              <tr>
+                <td>贡献 -1 点</td>
+                <td><a href="home.php?mod=space&amp;uid=43">Carol</a></td>
+                <td>2026-7-13 19:30</td>
+                <td></td>
+              </tr>
+            </table>
+        """.trimIndent()
+
+        val ratings = parsePostRatings(html)
+
+        assertEquals(2, ratings.size)
+        assertEquals("百合币", ratings[0].creditName)
+        assertEquals(3, ratings[0].score)
+        assertEquals("枚", ratings[0].unit)
+        assertEquals(42, ratings[0].userId)
+        assertEquals("Alice & Bob", ratings[0].username)
+        assertEquals("生日快乐 & 好图", ratings[0].reason)
+        assertEquals(-1, ratings[1].score)
+        assertEquals("", ratings[1].reason)
+    }
+
+    @Test
+    fun findsEveryRatedPostFromThreadPage() {
+        val html = """
+            <dl id="ratelog_41291769" class="rate"></dl>
+            <div id="post_rate_div_41291771"></div>
+            <DL class='rate' ID='ratelog_41291772'></DL>
+            <dl id="ratelog_invalid"></dl>
+        """.trimIndent()
+
+        assertEquals(setOf(41291769, 41291772), parseRatedPostIds(html))
+    }
+
     private companion object {
         val FIXTURE = """
           {
             "ppp":"20",
-            "thread":{"tid":"1000","author":"alice","authorid":"42","dateline":"10","digest":"0","fid":"300","attachment":"0","heats":"1","closed":"0","lastposter":"bob","lastpost":"刚刚","maxposition":"2","price":"0","readperm":"0","recommend_add":"1","replies":"1","special":"1","subject":"投票","typeid":"0","views":"12"},
-            "postlist":[{"author":"alice","authorid":"42","anonymous":"0","groupiconid":"","groupid":"10","pid":"9","dbdateline":"10","dateline":"刚刚","message":"<p>不可信正文</p>","attachment":"1","attachments":{"8":{"aid":"8","url":"data/attachment/forum/","attachment":"202607/example.jpg","filename":"example.jpg","isimage":"1"}},"first":"1","number":"1","position":"1","replycredit":"0","status":"0","tid":"1000"}],
+            "thread":{"tid":"1000","author":"alice","authorid":"42","dateline":"10","digest":"0","fid":"300","attachment":"0","heats":"1","rate":"1","closed":"0","lastposter":"bob","lastpost":"刚刚","maxposition":"2","price":"0","readperm":"0","recommend_add":"1","replies":"1","special":"1","subject":"投票","typeid":"0","views":"12"},
+            "postlist":[{"author":"alice","authorid":"42","anonymous":"0","groupiconid":"","groupid":"10","pid":"9","dbdateline":"10","dateline":"刚刚","message":"<p>不可信正文</p>","attachment":"1","attachments":{"8":{"aid":"8","url":"data/attachment/forum/","attachment":"202607/example.jpg","filename":"example.jpg","isimage":"1"}},"first":"1","number":"1","position":"1","ratetimes":"4","replycredit":"0","status":"0","tid":"1000"}],
             "comments":{"9":[{"author":"bob","authorid":"43","avatar":"//example.com/a.png","dateline":"刚刚","id":"2","comment":"点评","pid":"9","tid":"1000"}]},
             "special_poll":{"allowvote":"1","expirations":"0","maxchoices":"1","multiple":"0","visiblepoll":"1","voterscount":"4","polloptions":{"7":{"color":"ff00aa","polloptionid":"7","percent":"25.5","polloption":"选项","votes":"1"}}}
           }
